90df837b4436209ff59c7b6d9d3a12864d59948b
Paulina Szeszel Web Developer <86908540+PaulinaSzeszel@users.noreply.github.com>
2022-11-16 20:13:07 +0100

    created pages and card of components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,143 @@
-import { PureComponent } from 'react'
+import React, { Component } from 'react'
 import Header from './components/Header'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './Pages/Home'
+import Pdp from './Pages/Pdp'
+import { Query } from '@apollo/react-components'
+import { getAllCategories } from './query/getQueries'
+import Cart from './Pages/Cart'
 
-class App extends PureComponent {
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      orders: [],
+      currencyKey: 0,
+      itemNames: [],
+    }
+    this.addToOrder = this.addToOrder.bind(this)
+  }
+  componentDidMount() {
+    const currencyDropdown = document.querySelector('.dropdown-text')
+    document.addEventListener('click', () => {
+      switch (currencyDropdown.textContent.charAt(0)) {
+        case '£':
+          this.setState({
+            currencyKey: 1,
+          })
+          break
+        case 'A':
+          this.setState({
+            currencyKey: 2,
+          })
+          break
+        case '¥':
+          this.setState({
+            currencyKey: 3,
+          })
+          break
+        case '₽':
+          this.setState({
+            currencyKey: 4,
+          })
+          break
+        default:
+          this.setState({
+            currencyKey: 0,
+          })
+      }
+    })
+  }
+
   render() {
     return (
-      <div>
-        <Header />
+      <div className="App">
+        <div className="container">
+          <Query query={getAllCategories}>
+            {({ loading, data }) => {
+              if (loading) {
+                return <div>loading</div>
+              }
+              const { categories } = data
+              const { currencyKey, orders, itemNames } = this.state
+              console.log(currencyKey)
+              return (
+                <div>
+                  <Header
+                    currency={currencyKey}
+                    orders={orders}
+                    data={data}
+                    total={this.state.total}
+                  />
+                  <Routes>
+                    <Route path="/" element={<Navigate to="/all" />} />
+                    <Route
+                      path={`/${categories[0].name}`}
+                      element={
+                        <Home
+                          onAdd={this.addToOrder}
+                          categoryName={categories[0].name}
+                          currency={currencyKey}
+                        />
+                      }
+                    />
+                    <Route
+                      path={`/${categories[1].name}`}
+                      element={
+                        <Home
+                          currency={currencyKey}
+                          categoryName={categories[1].name}
+                        />
+                      }
+                    />
+                    <Route
+                      path={`/${categories[2].name}`}
+                      element={
+                        <Home
+                          currency={currencyKey}
+                          categoryName={categories[2].name}
+                        />
+                      }
+                    />
+
+                    <Route
+                      exact
+                      path="/product/:productID"
+                      element={
+                        <Pdp
+                          itemNames={itemNames}
+                          onAdd={this.addToOrder}
+                          currency={currencyKey}
+                        />
+                      }
+                    />
+                    <Route
+                      exact
+                      path="/cart"
+                      element={
+                        <Cart
+                          total={this.state.total}
+                          orders={this.state.orders}
+                          currency={currencyKey}
+                        />
+                      }
+                    />
+                  </Routes>
+                </div>
+              )
+            }}
+          </Query>
+        </div>
       </div>
     )
+  }
+
+  addToOrder = (item, itemName) => {
+    this.setState((prevState) => ({
+      orders: [...prevState.orders, item],
+      itemNames: [...this.state.itemNames, itemName],
+    }))
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+import { App } from './App'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './Redux/store'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
+  </BrowserRouter>
 )
